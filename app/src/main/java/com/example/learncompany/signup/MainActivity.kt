0513d8774a095f4996b26359c4ed60289be96700
Mohsen Farahanchi <mohsenfarahanchi@outlook.com>
2018-09-10 18:54:04 +0500

    add Snackbar and modify scrollView

--- app/src/main/java/com/example/learncompany/signup/MainActivity.kt
+++ app/src/main/java/com/example/learncompany/signup/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.learncompany.signup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val snack = Snackbar.make(it, editText.text.toString() + editText2.text + editText3.text, Snackbar.LENGTH_LONG)
+            snack.show()
+        }
     }
 }
